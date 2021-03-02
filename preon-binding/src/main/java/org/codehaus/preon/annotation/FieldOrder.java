cfc51bc6efebd0ac8232b872477a07206e1090ff
Angela DeGolier <adegolier@ctic-inc.com>
2021-03-02 14:17:47 -0500

    Added an annotation that allows field order to be specified

--- preon-binding/src/main/java/org/codehaus/preon/annotation/FieldOrder.java
+++ preon-binding/src/main/java/org/codehaus/preon/annotation/FieldOrder.java
@@ -12,12 +12,17 @@
 
 package org.codehaus.preon.annotation;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 /**
- * TODO: class description
+ * Allows the field order to be specified.
  *
  * @author Copyright &#169; 2021 Chesapeake Technology International Corp.
  * @since
  */
+@Retention(RetentionPolicy.RUNTIME)
 public @interface FieldOrder
 {
+    int index();
 }
