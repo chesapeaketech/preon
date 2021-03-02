1bc03bff3dde060c7bf155d8a75e65ad644e99f8
Angela DeGolier <adegolier@ctic-inc.com>
2021-03-02 14:07:19 -0500

    Added an annotation that allows field order to be specified

---
+++ preon-binding/src/main/java/org/codehaus/preon/annotation/FieldOrder.java
@@ -0,0 +1,23 @@
+//========================================================================
+//
+//                       U N C L A S S I F I E D
+//
+//========================================================================
+//  Copyright (c) 2021 Chesapeake Technology International Corp.
+//  ALL RIGHTS RESERVED
+//  This material may be reproduced by or for the U.S. Government
+//  pursuant to the copyright license under the clause at
+//  DFARS 252.227-7013 (OCT 1988).
+//=======================================================================
+
+package org.codehaus.preon.annotation;
+
+/**
+ * TODO: class description
+ *
+ * @author Copyright &#169; 2021 Chesapeake Technology International Corp.
+ * @since
+ */
+public @interface FieldOrder
+{
+}
