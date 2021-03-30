a7c03902b4a9e12a2701826bd9736c861472b606
Angela DeGolier <adegolier@ctic-inc.com>
2021-03-29 22:24:53 -0400

    Missed this one switching back to the VOLTRON code style.

--- preon-binding/src/main/java/org/codehaus/preon/codec/NumericType.java
+++ preon-binding/src/main/java/org/codehaus/preon/codec/NumericType.java
@@ -10,75 +10,75 @@ public enum NumericType implements INumericType
 {
 
     Float
-     {
-         public int getDefaultSize()
-         {
-             return 32;
-         }
+            {
+                public int getDefaultSize()
+                {
+                    return 32;
+                }
 
-         public Float decode(BitBuffer buffer, int size, ByteOrder endian)
-         {
-             int value = buffer.readAsInt(size, endian);
-             return java.lang.Float.intBitsToFloat(value);
-         }
+                public Float decode(BitBuffer buffer, int size, ByteOrder endian)
+                {
+                    int value = buffer.readAsInt(size, endian);
+                    return java.lang.Float.intBitsToFloat(value);
+                }
 
-         //TODO handle io exceptions on encode
-         public void encode(BitChannel channel, int size, ByteOrder endian, Object value) throws IOException
-         {
-             channel.write(size, (Float) value, endian);
-         }
+                //TODO handle io exceptions on encode
+                public void encode(BitChannel channel, int size, ByteOrder endian, Object value) throws IOException
+                {
+                    channel.write(size, (Float) value, endian);
+                }
 
-         public Class<?> getType()
-         {
-             return Float.class;
-         }
+                public Class<?> getType()
+                {
+                    return Float.class;
+                }
 
-         @Override
-         public Class<?> getPrimitiveType()
-         {
-             return java.lang.Float.TYPE;
-         }
+                @Override
+                public Class<?> getPrimitiveType()
+                {
+                    return java.lang.Float.TYPE;
+                }
 
-         @Override
-         public Class<?>[] getNumericTypes()
-         {
-             return new Class<?>[] { getType() };
-         }
-     },
+                @Override
+                public Class<?>[] getNumericTypes()
+                {
+                    return new Class<?>[]{getType()};
+                }
+            },
 
     Double
-     {
-         public int getDefaultSize()
-         {
-             return 64;
-         }
+            {
+                public int getDefaultSize()
+                {
+                    return 64;
+                }
 
-         public Double decode(BitBuffer buffer, int size, ByteOrder endian)
-         {
-             return java.lang.Double.longBitsToDouble(buffer.readAsLong(
-              size, endian));
-         }
+                public Double decode(BitBuffer buffer, int size, ByteOrder endian)
+                {
+                    return java.lang.Double.longBitsToDouble(buffer.readAsLong(
+                            size, endian));
+                }
 
-         public void encode(BitChannel channel, int size, ByteOrder endian, Object value) throws IOException
-         {
-             channel.write(size, (Double) value, endian);
-         }
+                public void encode(BitChannel channel, int size, ByteOrder endian, Object value) throws IOException
+                {
+                    channel.write(size, (Double) value, endian);
+                }
 
-         public Class<?> getType()
-         {
-             return Double.class;
-         }
+                public Class<?> getType()
+                {
+                    return Double.class;
+                }
 
-         @Override
-         public Class<?> getPrimitiveType()
-         {
-             return java.lang.Double.TYPE;
-         }
+                @Override
+                public Class<?> getPrimitiveType()
+                {
+                    return java.lang.Double.TYPE;
+                }
 
-         @Override
-         public Class<?>[] getNumericTypes()
-         {
-             return new Class<?>[] { getType() };
-         }
-     },
+                @Override
+                public Class<?>[] getNumericTypes()
+                {
+                    return new Class<?>[]{getType()};
+                }
+            },
 }
